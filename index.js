f90c6be2c0bc5d53c82252cfef992235b43a971b
David <david.gawin@gmail.com>
2024-12-12 15:53:01 +0100

    Loading speed fix
    Changelog:
    - added loading of app seperate from login

--- index.js
+++ index.js
@@ -22,17 +22,37 @@ function changePage(page){
     getEl("startPage").style.display = "none";
     getEl("loginPage").style.display = "none";
     getEl("registerPage").style.display = "none";
-    getEl("app").style.display = "none";
+    if (getEl("app") != null){
+        getEl("app").style.display = "none";
+    }
+
     newPage.style.display = "block";
     
     console.log(`Successfully changed to page [${page}]`)
 }
 
-function userLoad(){
-    getEl("loading").style.display = "none"
-    if (!loggedIn){
-        changePage("startPage")
-    }else{
+function appLoaded(){
+    if (loggedIn){
+        getEl("loading").style.display = "none" 
         changePage("app")
     }
+
+}
+
+function userLoad(){
+    
+    if (!loggedIn){
+        changePage("startPage")
+        getEl("loading").style.display = "none"
+    }
+    document.body.insertBefore(createAppEl(), document.getElementById("script"))
+}
+
+function createAppEl(){
+    let newEl = document.createElement("iframe")
+    newEl.src = "app/app.html"
+    newEl.id = "app"
+    newEl.frameBorder = 0;
+    newEl.style.display = "none";
+    return newEl;
 }
