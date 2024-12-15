9803ea993206b517272fae5614c023d874da8610
David <david.gawin@gmail.com>
2024-12-15 13:54:45 +0100

    Swipe function
    Changelog:
    - Added swipe action on swipe page

---
+++ app/swipe/swipeLogic.js
@@ -0,0 +1,41 @@
+const swipeArea = document.getElementById("swipeZone");
+const swipeParent = swipeArea.parentElement
+
+let startX = 0;
+let isSwiping = false;
+
+swipeArea.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX; // Record the initial touch position
+    isSwiping = true;
+});
+
+swipeArea.addEventListener("touchmove", (e) => {
+    if (!isSwiping) return;
+    swipeParent.style.transition = ""
+    e.preventDefault();
+
+    const currentX = e.touches[0].clientX;
+    const diffX = currentX - startX;
+    swipeParent.style.transform = `translateX(${diffX/4}%) rotate(${diffX/30}deg)`
+    console.log(swipeParent)
+    // Optionally show swipe progress here
+});
+
+swipeArea.addEventListener("touchend", (e) => {
+    if (!isSwiping) return;
+    swipeParent.style.transition = "transform 0.1s" 
+    isSwiping = false;
+
+    const endX = e.changedTouches[0].clientX;
+    const diffX = endX - startX;
+    const threshold = 200;
+
+    if (diffX > threshold) {
+        swipeParent.style.transform = `translateX(120%) rotate(13deg)`
+    } else if (diffX < -threshold) {
+        swipeParent.style.transform = `translateX(-120%) rotate(-13deg)`
+        
+    } else {
+        swipeParent.style.transform = `translateX(0px) rotate(0deg)`
+    }
+});
